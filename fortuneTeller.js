19769b24aecf189d728cd6da43250eda8b7b5f2c
KOSSY <kossy@KOSSYs-MacBook-Pro.local>
2022-02-20 14:42:06 +0100

    updated

---
+++ fortuneTeller.js
@@ -0,0 +1,13 @@
+/*Store the following into variables: number of children, partner's name, 
+geographic location, job title. Output your fortune to the screen like so: 
+"You will be a X in Y, and married to Z with N kids."*/
+
+const noOfChildren = 4;
+const partnerName = "Somke Abone";
+const location = "America";
+const jobTitle = "Software Engineer"
+
+console.log(`
+My fortune: 
+I'll be a ${jobTitle} in ${location}, and married to ${partnerName} with ${noOfChildren} kids.
+`)
