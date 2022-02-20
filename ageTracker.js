60fee4492e93bc02718a30f99a455ed9dff3205c
KOSSY <kossy@KOSSYs-MacBook-Pro.local>
2022-02-20 15:05:46 +0100

    updated

---
+++ ageTracker.js
@@ -0,0 +1,11 @@
+/*Want to find out how old you'll be? Calculate it!
+Store your birth year in a variable.
+Store a future year in a variable.
+Calculate your 2 possible ages for that year based on the stored values.*/
+
+function checkFutureAge (birthYear, futureYear){
+    let futureAge = futureYear - birthYear;
+    console.log(`I'll be either ${futureAge - 1} or ${futureAge} in ${futureYear}.\n`)
+}
+
+checkFutureAge(1993, 2022);
